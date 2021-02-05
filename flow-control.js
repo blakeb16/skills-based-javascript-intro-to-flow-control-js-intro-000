c11c51a63021187074686d5dbd475874e1f21562
Blake Busby <blakebusby16@gmail.com>
2021-02-05 20:45:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,3 +27,15 @@ function ageChecker(age){
 function ternaryTeenager(age){
   return (age >=13 && age <= 19) ? "You are a teenager": "You are not a teenager"
 }
+
+function switchAge(age){
+
+  switch(age){
+    case true:
+      return "You are a teenager"
+    default:
+      return "You have an age"
+    
+    
+  }
+}
